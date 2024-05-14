c412766013da04029e5022d9b4d8cc8b9e0e5dbb
addrikiran <58554817+addrikiran@users.noreply.github.com>
2024-05-14 12:21:47 +0530

    Add files via upload

---
+++ WEBPAGES_FINAL/welcomePage.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+  window.location.href = "login.html"; // Redirect to login page
+});
+
+document.getElementById("registerBtn").addEventListener("click", function() {
+  window.location.href = "register.html"; // Redirect to register page
+});
