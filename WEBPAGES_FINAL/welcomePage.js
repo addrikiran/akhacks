5d47fc54752a075cd495225efa7b637eb96de587
addrikiran <58554817+addrikiran@users.noreply.github.com>
2024-05-16 20:36:50 +0530

    Delete WEBPAGES_FINAL directory

--- WEBPAGES_FINAL/welcomePage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("loginBtn").addEventListener("click", function() {
-  window.location.href = "login.html"; // Redirect to login page
-});
-
-document.getElementById("registerBtn").addEventListener("click", function() {
-  window.location.href = "register.html"; // Redirect to register page
-});
