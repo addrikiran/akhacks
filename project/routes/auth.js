56a9a5bc7248954c7f9f122c3d7b49b4b0696a61
addrikiran <58554817+addrikiran@users.noreply.github.com>
2024-05-17 09:49:11 +0530

    Delete project directory

--- project/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const User = require('project/models/User');
-
-// Authentication routes
-// Define your authentication routes here
-
-module.exports = router;
